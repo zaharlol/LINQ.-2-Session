f621bdd275317a058b6d7f5a44242513264b64b2
zaharlol <kovtun.zahar12@mail.ru>
2023-06-06 15:24:18 +0300

    Проверка выполнения

--- Join/Program.cs
+++ Join/Program.cs
@@ -4,7 +4,36 @@ class Program
 {
     static void Main(string[] args)
     {
+        List<int> a = new List<int> { 1, 2, 3, 4 };
 
+        var c = a.Where(c => c > 2).ToArray();
+        a.Add(6);
+
+        Console.WriteLine("Добавляем эллемент после выполнения");
+        foreach (var t in c)
+        {
+            Console.WriteLine(t);
+        }
+
+        a.Add(6);
+        var n = a.Where(b => b > 2).ToArray();
+
+        Console.WriteLine("А тут до выполнения");
+        foreach (var t in c)
+        {
+            Console.WriteLine(t);
+        }
+
+        Console.WriteLine("Итоговое значение нашей коллекции");
+
+        foreach (var t in a)
+        Console.WriteLine(t);
+    }
+}
+class Dep
+{
+    public void Dep1()
+    {
         var departments = new List<Department>()
 {
    new Department() {Id = 1, Name = "Программирование"},
@@ -28,7 +57,7 @@ class Program
                       Id = exp.Id,
                   };
 
-        foreach (var a in cow) 
+        foreach (var a in cow)
         {
             Console.WriteLine(a.Name + " " + a.NameN + " " + a.Id);
         }
